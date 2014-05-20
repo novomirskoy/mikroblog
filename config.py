3273345672bbf36a6cef29ef6dcea66726f84913
Artyom <artyom.endin@gmail.com>
2014-05-20 23:51:37 +0400

    модификация файлов

--- config.py
+++ config.py
@@ -3,8 +3,5 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "blog.db")
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")
-
 WTF_CSRF_ENABLED = True
 SECRET_KEY = "blablublimblimblubla"
